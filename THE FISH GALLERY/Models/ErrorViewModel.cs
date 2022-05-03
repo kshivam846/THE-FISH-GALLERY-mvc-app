829b06d2e55c69600e5245b056804eb38e405349
hp <hp@DESKTOP-QLSITJ2>
2022-05-02 18:13:07 -0700

    Add project files.

---
+++ THE FISH GALLERY/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace THE_FISH_GALLERY.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
